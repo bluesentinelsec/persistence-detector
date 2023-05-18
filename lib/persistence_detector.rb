9c86368db090b31914ac75699b3a7aa4d0b291b9
bluesentinelsec <bluesentinel@protonmail.com>
2023-05-18 00:57:44 -0400

    added detectors module

--- lib/persistence_detector.rb
+++ lib/persistence_detector.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 module PersistenceDetector
-  # Client docs TBD
+
   class Client
-    def self.client
-      puts 'hello from PersistenceDetector::Client.client()'
+    @logger
+
+    def initialize(logger)
+      @logger = logger
+    end
+
+    def DetectPersistenceTTP(ttp)
+      if ttp == 'all' || ttp == ""
+        Detectors::DETECTORS.each do |detector|
+          puts("Invoke detector: #{detector}")
+        end
+      end
+    end
+    def ListDetectors
+      Detectors::DETECTORS.each do |detector|
+        puts(detector)
+      end
     end
   end
 end
 
 require 'persistence_detector/widget'
+require 'persistence_detector/detectors'
---
+++ lib/persistence_detector/detectors.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Detectors
+  DETECTORS = ["todo1", "todo2", "todo3"]
+end
